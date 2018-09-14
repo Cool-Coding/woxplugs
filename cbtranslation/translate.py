0e0d66ba901e818477fd8d668f9a95559a7e6c94
cool-coding <riguang_2007@163.com>
2018-09-14 17:59:17 +0800

    add empty judge
    添加了没有查询到消息显示和跳转到相应的爱词霸页面，了解详细信息

--- cbtranslation/translate.py
+++ cbtranslation/translate.py
@@ -1,8 +1,8 @@
 # encoding=utf8
 import requests
 from bs4 import BeautifulSoup
-from wox import Wox
-
+import webbrowser
+from wox import Wox,WoxAPI
 
 # 用户写的Python类必须继承Wox类 https://github.com/qianlifeng/Wox/blob/master/PythonHome/wox.py
 # 这里的Wox基类做了一些工作，简化了与Wox通信的步骤。
@@ -14,6 +14,10 @@ class Main(Wox):
                       '(Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 ' \
                       '(KHTML, like Gecko) Version/5.1 Safari/534.50'
         return requests.get(url,headers = heads)
+
+    def openUrl(self,url):
+        webbrowser.open(url)
+        WoxAPI.change_query(url)
 
     # 必须有一个query方法，用户执行查询的时候会自动调用query方法
     def query(self, key):
@@ -32,12 +36,24 @@ class Main(Wox):
             results.append({
               "Title": text,
               "SubTitle": subtext,
-              "IcoPath": "Images/app.ico"
+              "IcoPath": "Images/app.ico",
+              "JsonRPCAction":{
+                 #这里除了自已定义的方法，还可以调用Wox的API。调用格式如下：Wox.xxxx方法名
+                 #方法名字可以从这里查阅https://github.com/qianlifeng/Wox/blob/master/Wox.Plugin/IPublicAPI.cs 直接同名方法即可
+                 "method": "openUrl",
+                 #参数必须以数组的形式传过去
+                 "parameters":[url],
+                 #是否隐藏窗口
+                 "dontHideAfterAction":True
+                }
             })
-        f = open("E:/out.txt",'w')
-        for i in results:
-            f.write(i.get("Title"))
-            
+       
+        if len(results)==0:
+            results.append({
+                "Title": "oops! 没有查询到",
+                "SubTitle": '',
+                "IcoPath": "Images/app.ico"
+            })
         return results
 
 
